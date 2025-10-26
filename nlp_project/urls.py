0f49971f2b6cdac7cceac16fe85813845da6e078
wares <mw@tibxtech.com>
2025-10-26 19:38:04 +0430

    last code

--- nlp_project/urls.py
+++ nlp_project/urls.py
@@ -6,5 +6,6 @@ import nltk
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
-    path("about/", views.about, name="About page")
+    path("about/", views.about, name="About page"),
+    path("get-data/",views.get_details, name="Getting and parsing data")
 ]
--- nlp_project/views.py
+++ nlp_project/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
 
 def home(request):
     return render(request, "index.html")
 
 def about(request):
     return render(request, "about.html")
+
+
+
+
+def get_details(request):
+    text = request.POST['text'];
+
+    return JsonResponse(text, safe=False)
